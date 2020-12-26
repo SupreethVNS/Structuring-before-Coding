8450e6157606a82ee31b57c905543aaaf706bb68
SupreethVNS <74822785+SupreethVNS@users.noreply.github.com>
2020-12-26 19:06:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var hypnoticBall, database;
+var position;
+var form,player,game;
+var gameState=0;
+var playerCount=0;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+  game = new Game();
+  game.getState();
+  game.start();
+
+}
+
+function draw(){
+  background("white");
+  
+   
+    drawSprites();
+  
+}
+
+
